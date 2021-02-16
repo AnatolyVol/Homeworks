4dd35247cb8bf2dd50ddbe2a82b701a75a3e2902
AnatolyVol <60568105+AnatolyVol@users.noreply.github.com>
2021-02-17 01:18:06 +0400

    Homework2

--- src/Main.java
+++ src/Main.java
@@ -1,72 +1,47 @@
 public class Main {
-//Задание 1
     public static void main(String[] args) {
+//Задание 1
+        int[] arr = {0, 1, 1, 1, 0, 1, 1, 0, 0, 0};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else if (arr[i] == 1) {
+                arr[i] = 0;
+            }
+        }
+        System.out.println(arr[0]);
+
 //Задание 2
-        byte aaaa = -127;
-        short bbbb = 32654;
-        int cccc = -7876553;
-        long dddd = 654659849;
+        int[] arr1 = new int[8];
+        for (int i = 1; i < arr1.length; i++) {
+            arr1[i] = arr1[i - 1] + 3;
+        }
+        System.out.println(arr1[7]);
 
-        float eeee = 4.55f;
-        double ffff = -234.12;
-
-        char gggg = '#';
-
-        boolean hhhh = true;
-        boolean iiii = false;
 //Задание 3
-        float a = 1.1f;
-        float b = 2.2f;
-        float c = 3.3f;
-        float d = 4.4f;
-        System.out.println(a * (b + (c / d)));
+        int[] arr2 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr2.length; i++) {
+            if (arr2[i] < 6) {
+                arr2[i] = arr2[i] * 2;
+            }
+        }
+        System.out.println(arr2[0]);
 
 //Задание 4
-        int aa = 7;
-        int bb = 20;
-        int cc = aa + bb;
-        if (10 <= cc && cc <= 20) {
-            System.out.println(true);
-        } else if (cc < 10 || cc > 20) {
-            System.out.println(false);
+        int[][] twoArr = new int[11][11];
+        for (int i = 0; i < twoArr.length; i++){
+            for (int j = 0; j < twoArr[i].length; j++){
+                if (i == j){
+                    twoArr[i][j] = 1;
+                    twoArr[i][twoArr.length - 1 - i] = 1;
+                }
+            }
         }
-//Задание 5
-        int dd = 15;
-        if (dd >= 0) {
-            System.out.println("Положительное");
-        } else if (dd < 0) {
-            System.out.println("Отрицательное");
-        }
-//Задание 6
-        int ff = -3;
-        if (ff > 0) {
-            System.out.println(false);
-        } else if (ff < 0) {
-            System.out.println(true);
-        }
-//Задание 7
-        String mm = "AnatolyVol";
-        System.out.println("Привет, " + mm + "!");
-//Задание 8
-        int year = 2504;
-        double x = year % 4;
-        double y = year % 100;
-        double z = year % 400;
-        if (z <= 0 && y <= 0 || x <= 0 && z <= 0 )
-        {
-            System.out.println("Високосный");
-        }
-        else if (y <= 0 )
-        {
-            System.out.println("Невисокосный");
-        }
-        else if ( x > 0)
-        {
-            System.out.println("Невисокосный");
-        }
-        else if ( x <= 0)
-        {
-            System.out.println("Високосный");
+        for (int i = 0; i < twoArr.length; i++) {
+            for (int j = 0; j < twoArr[i].length; j++){
+                System.out.print(twoArr[i][j]);
+            }
+            System.out.println( );
         }
     }
 }
