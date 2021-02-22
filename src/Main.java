85a3d5c6a33cc9ab4ffa97d403b4f99a98fefa24
AnatolyVol <60568105+AnatolyVol@users.noreply.github.com>
2021-02-22 19:23:45 +0400

    Homework3

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,61 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
 public class Main {
-    public static void main(String[] args) {
-//Задание 1
-        int[] arr = {0, 1, 1, 1, 0, 1, 1, 0, 0, 0};
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0) {
-                arr[i] = 1;
-            } else if (arr[i] == 1) {
-                arr[i] = 0;
+    public static void main(String[] args)
+    {
+        int i = 0;
+        int number = randomNumber();
+        System.out.println("Введи число");
+        while (i <= 2)
+        {
+            int guessNumber = readConsole();
+            int diff = number - guessNumber;
+            if (i == 2)
+            {
+                System.out.println("Проиграл :(");
+                break;
             }
+            if (diff == 0)
+            {
+                System.out.println("Молодец, угадал!");
+                break;
+            }
+            if (diff < 0)
+            {
+                System.out.println("Слишком много!");
+            }
+            else if (diff > 0)
+            {
+                System.out.println("Бери больше!");
+            }
+            i++;
         }
-        System.out.println(arr[0]);
+        System.out.println("Повторить игру еще раз? (1 – повторить, 0 – нет)");
 
-//Задание 2
-        int[] arr1 = new int[8];
-        for (int i = 1; i < arr1.length; i++) {
-            arr1[i] = arr1[i - 1] + 3;
-        }
-        System.out.println(arr1[7]);
-
-//Задание 3
-        int[] arr2 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = 0; i < arr2.length; i++) {
-            if (arr2[i] < 6) {
-                arr2[i] = arr2[i] * 2;
-            }
-        }
-        System.out.println(arr2[0]);
-
-//Задание 4
-        int[][] twoArr = new int[11][11];
-        for (int i = 0; i < twoArr.length; i++){
-            for (int j = 0; j < twoArr[i].length; j++){
-                if (i == j){
-                    twoArr[i][j] = 1;
-                    twoArr[i][twoArr.length - 1 - i] = 1;
-                }
-            }
-        }
-        for (int i = 0; i < twoArr.length; i++) {
-            for (int j = 0; j < twoArr[i].length; j++){
-                System.out.print(twoArr[i][j]);
-            }
-            System.out.println( );
-        }
     }
-}
+    static int randomNumber()
+    {
+        Random rand = new Random();
+        int number1 = rand.nextInt(10);
+        return number1;
+    }
+    static int readConsole()
+    {
+        Scanner sc = new Scanner(System.in);
+            do
+            {
+                if (sc.hasNextInt())
+                {
+                    return sc.nextInt();
+                }
+                sc.nextLine();
+            }
+            while (true);
+    }
+
+
+
+    }
+
