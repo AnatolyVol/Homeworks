5b94f50e5d080be2c22fa10ec423b7185c9b5e20
AnatolyVol <60568105+AnatolyVol@users.noreply.github.com>
2021-02-25 18:27:59 +0400

    Homework4

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,104 @@
 import java.util.Random;
 import java.util.Scanner;
 
-
 public class Main {
-    public static void main(String[] args)
-    {
-        int i = 0;
-        int number = randomNumber();
-        System.out.println("Введи число");
-        while (i <= 2)
-        {
-            int guessNumber = readConsole();
-            int diff = number - guessNumber;
-            if (i == 2)
-            {
-                System.out.println("Проиграл :(");
+    public static int SIZE = 3;
+    public static int DOTS_TO_WIN = 3;
+    public static final char DOT_EMPTY = '•';
+    public static final char DOT_X = 'X';
+    public static final char DOT_O = 'O';
+    public static char[][] map;
+    public static Scanner sc = new Scanner(System.in);
+    public static Random rand = new Random();
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Ты победил");
                 break;
             }
-            if (diff == 0)
-            {
-                System.out.println("Молодец, угадал!");
+            if (isMapFull()) {
+                System.out.println("Попробуй ещё раз");
                 break;
             }
-            if (diff < 0)
-            {
-                System.out.println("Слишком много!");
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Ты проиграл");
+                break;
             }
-            else if (diff > 0)
-            {
-                System.out.println("Бери больше!");
+            if (isMapFull()) {
+                System.out.println("Попробуй ещё раз");
+                break;
             }
-            i++;
         }
-        System.out.println("Повторить игру еще раз? (1 – повторить, 0 – нет)");
-
+        System.out.println("Игра закончена");
     }
-    static int randomNumber()
-    {
-        Random rand = new Random();
-        int number1 = rand.nextInt(10);
-        return number1;
+    public static boolean checkWin(char symb) {
+        if(map[0][0] == symb && map[0][1] == symb && map[0][2] == symb) return true;
+        if(map[1][0] == symb && map[1][1] == symb && map[1][2] == symb) return true;
+        if (map[2][0] == symb && map[2][1] == symb && map[2][2] == symb) return true;
+        if (map[0][0] == symb && map[1][0] == symb && map[2][0] == symb) return true;
+        if (map[0][1] == symb && map[1][1] == symb && map[2][1] == symb) return true;
+        if (map[0][2] == symb && map[1][2] == symb && map[2][2] == symb) return true;
+        if (map[0][0] == symb && map[1][1] == symb && map[2][2] == symb) return true;
+        if (map[2][0] == symb && map[1][1] == symb && map[0][2] == symb) return true;
+        return false;
     }
-    static int readConsole()
-    {
-        Scanner sc = new Scanner(System.in);
-            do
-            {
-                if (sc.hasNextInt())
-                {
-                    return sc.nextInt();
-                }
-                sc.nextLine();
+    public static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) return false;
             }
-            while (true);
+        }
+        return true;
     }
-
-
-
+    public static void aiTurn() {
+        int x, y;
+        do {
+            x = rand.nextInt(SIZE);
+            y = rand.nextInt(SIZE);
+        } while (!isCellValid(x, y));
+        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
+        map[y][x] = DOT_O;
     }
-
+    public static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты в формате X Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
+        map[y][x] = DOT_X;
+    }
+    public static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+        if (map[y][x] == DOT_EMPTY) return true;
+        return false;
+    }
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+    public static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
